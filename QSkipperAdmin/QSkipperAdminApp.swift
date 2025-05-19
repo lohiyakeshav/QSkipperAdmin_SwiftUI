18b5e9847fb6e67efb030dd5924c1798d84d9093
Keshav Lohiya <keshav.lohiyas@gmail.com>
2025-05-19 23:24:10 +0530

    Initial Commit

---
+++ QSkipperAdmin/QSkipperAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  QSkipperAdminApp.swift
+//  QSkipperAdmin
+//
+//  Created by Keshav Lohiya on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct QSkipperAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
